9c27f72d06099539ec32bafb5e473d5873897ca6
einarformica-code <einar.formica@iesnervion.es>
2026-04-09 14:39:18 +0200

    ----Primer día interfaces

---
+++ tema5Herencias_Interfaces/src/InterfacesDia1/PRINSIPALL.java
@@ -0,0 +1,15 @@
+package InterfacesDia1;
+
+public class PRINSIPALL {
+public static void main(String[] args) {
+	
+	Perro perro = new Perro("Doggo", "Bulldog", 45, "negro");
+	Gato gato = new Gato("Misi","Multiorgasmica", 15, "Rojo");
+	
+	mostrarPerro(perro);
+	mostrarGato(gato);
+	
+	demostrarPolimorfismoAnimal(new Animal)
+	
+}		
+}
---
+++ tema5Herencias_Interfaces/src/InterfacesDia1/Vivir.java
@@ -0,0 +1,11 @@
+package InterfacesDia1;
+
+public interface Vivir {
+	
+	void comer();
+	
+	void dormir();
+	
+	void hacerRuido();
+
+}
